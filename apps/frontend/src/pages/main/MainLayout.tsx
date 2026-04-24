2ea3205e95764945768b0a37ad0a294f3143af03
SAOOD <saood-aslam@outlook.com>
2026-04-24 14:00:15 +0100

    updating bottom nav bar

--- apps/frontend/src/pages/main/MainLayout.tsx
+++ apps/frontend/src/pages/main/MainLayout.tsx
@@ -1,4 +1,4 @@
-import { ArrowBack, HomeOutlined, SettingsOutlined } from '@mui/icons-material';
+import { ArrowBack, BedtimeOutlined, LightModeOutlined, LocalOfferOutlined, SettingsOutlined } from '@mui/icons-material';
 import { AppBar, BottomNavigation, BottomNavigationAction, Box, Container, IconButton, Toolbar, Typography } from '@mui/material';
 import { Outlet, useMatches, useNavigate } from 'react-router';
 import { ROUTE_PATHS, type T_Route_Path, type T_Route_UseMatches } from '../../routes/router';
@@ -30,13 +30,14 @@ export default function MainLayout(): React.JSX.Element {
          <Container>
             <Outlet />
          </Container>
-
          {!currentMatch?.handle?.nav?.hide && (
             <BottomNavigation
                value={matches.findLast((match) => match.handle?.nav?.inBottomNav)?.pathname ?? false}
                onChange={(_, newValue: T_Route_Path) => void navigate(newValue)}
             >
-               <BottomNavigationAction label={'Home'} value={ROUTE_PATHS.main_home} icon={<HomeOutlined />} />
+               <BottomNavigationAction label={'Morning'} value={ROUTE_PATHS.main_routine_morning} icon={<LightModeOutlined />} />
+               <BottomNavigationAction label={'Evening'} value={ROUTE_PATHS.main_routine_evening} icon={<BedtimeOutlined />} />
+               <BottomNavigationAction label={'Tags'} value={ROUTE_PATHS.main_tags} icon={<LocalOfferOutlined />} />
                <BottomNavigationAction label={'Settings'} value={ROUTE_PATHS.main_settings} icon={<SettingsOutlined />} />
             </BottomNavigation>
          )}
