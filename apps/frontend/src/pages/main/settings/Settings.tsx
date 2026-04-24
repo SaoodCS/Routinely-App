831ce48985b7bbc55dfb8addec2f86b60f60ac90
SAOOD <saood-aslam@outlook.com>
2026-04-24 23:06:33 +0100

    revert settings

--- apps/frontend/src/pages/main/settings/Settings.tsx
+++ apps/frontend/src/pages/main/settings/Settings.tsx
@@ -1,8 +1,4 @@
 import { Box } from '@mui/material';
-import { useState } from 'react';
-import DragDropWrapper from '../../../components/DragDropWrapper';
-import ScrollRestoreWrapper from '../../../components/ScrollRestoreWrapper';
-import SwipeActionWrapper from '../../../components/SwipeActionWrapper';
 
 const itemDummyData: { id: number; name: string; description: string }[] = [
    { id: 11, name: 'Item 1', description: 'Description of item 1' },
@@ -33,47 +29,5 @@ const subItemDummyData: { [key: number]: typeof itemDummyData } = {
 };
 
 export default function Settings(): React.JSX.Element {
-   const [items, setItems] = useState<typeof itemDummyData>(itemDummyData);
-   const [subItems, setSubItems] = useState<typeof subItemDummyData>(subItemDummyData);
-
-   function handleParentDrop(newOrderedArr: typeof itemDummyData): void {
-      // function that also updates the order in the database
-      setItems(newOrderedArr);
-   }
-
-   function handleChildDrop(newOrderedArr: (typeof subItemDummyData)[number], parentId: number): void {
-      // function that also updates the order in the database
-      setSubItems({ ...subItems, [parentId]: newOrderedArr });
-   }
-
-   return (
-      <ScrollRestoreWrapper storeKey="settings lol" sx={{ height: '100%', overflow: 'auto' }}>
-         <DragDropWrapper
-            items={items}
-            getKey={(item) => item.id}
-            onDrop={(newOrderedArr) => handleParentDrop(newOrderedArr)}
-            renderItem={(item) => (
-               <Box>
-                  <SwipeActionWrapper>
-                     <Box>{item.name}</Box>
-                     <Box>{item.description}</Box>
-                  </SwipeActionWrapper>
-                  {subItems[item.id] && (
-                     <DragDropWrapper
-                        items={subItems[item.id]}
-                        getKey={(subItem) => subItem.id}
-                        onDrop={(newOrderedArr) => handleChildDrop(newOrderedArr, item.id)}
-                        renderItem={(subItem) => (
-                           <Box>
-                              <Box>{subItem.name}</Box>
-                              <Box>{subItem.description}</Box>
-                           </Box>
-                        )}
-                     />
-                  )}
-               </Box>
-            )}
-         />
-      </ScrollRestoreWrapper>
-   );
+   return <Box>Setiings</Box>;
 }
