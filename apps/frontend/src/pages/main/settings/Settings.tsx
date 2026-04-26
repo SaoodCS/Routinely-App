4fbc00fca1f9f65ae5bc184dad6827662f6138df
SAOOD <saood-aslam@outlook.com>
2026-04-27 00:06:55 +0100

    updat settings style

--- apps/frontend/src/pages/main/settings/Settings.tsx
+++ apps/frontend/src/pages/main/settings/Settings.tsx
@@ -8,7 +8,7 @@ export default function Settings(): React.JSX.Element {
    }
 
    return (
-      <Stack height="100%" overflow={'auto'} alignItems={'center'} padding="1rem">
+      <Stack height="100%" overflow={'auto'} alignItems={'center'} padding="1rem" gap="1rem">
          <Paper sx={{ width: '100%', borderRadius: '1rem' }}>
             <MenuList dense>
                <MenuItem onClick={resetAllData}>
