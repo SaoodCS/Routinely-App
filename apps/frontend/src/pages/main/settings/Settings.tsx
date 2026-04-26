7ebc29cd93570205bbb75b82fb0f443c9ed827d0
SAOOD <saood-aslam@outlook.com>
2026-04-27 00:06:01 +0100

    paper component added to settings page

--- apps/frontend/src/pages/main/settings/Settings.tsx
+++ apps/frontend/src/pages/main/settings/Settings.tsx
@@ -1,7 +1,6 @@
 import { DeleteOutlineOutlined, RestartAltOutlined } from '@mui/icons-material';
-import { Divider, ListItemIcon, ListItemText, MenuItem, MenuList, Stack } from '@mui/material';
+import { Divider, ListItemIcon, ListItemText, MenuItem, MenuList, Paper, Stack } from '@mui/material';
 import type React from 'react';
-import { lighten } from '@mui/material/styles';
 
 export default function Settings(): React.JSX.Element {
    function resetAllData(): void {
@@ -10,29 +9,23 @@ export default function Settings(): React.JSX.Element {
 
    return (
       <Stack height="100%" overflow={'auto'} alignItems={'center'} padding="1rem">
-         <MenuList
-            dense
-            sx={{
-               width: '100%',
-               bgcolor: ({ palette }) => lighten(palette.background.default, 0.05),
-               borderRadius: '1rem',
-               boxShadow: '0 0 1rem rgba(0, 0, 0, 0.2)',
-            }}
-         >
-            <MenuItem onClick={resetAllData}>
-               <ListItemIcon>
-                  <RestartAltOutlined />
-               </ListItemIcon>
-               <ListItemText>Reset All Data</ListItemText>
-            </MenuItem>
-            <Divider />
-            <MenuItem>
-               <ListItemIcon>
-                  <DeleteOutlineOutlined />
-               </ListItemIcon>
-               <ListItemText>Delete Account</ListItemText>
-            </MenuItem>
-         </MenuList>
+         <Paper sx={{ width: '100%', borderRadius: '1rem' }}>
+            <MenuList dense>
+               <MenuItem onClick={resetAllData}>
+                  <ListItemIcon>
+                     <RestartAltOutlined />
+                  </ListItemIcon>
+                  <ListItemText>Reset All Data</ListItemText>
+               </MenuItem>
+               <Divider />
+               <MenuItem>
+                  <ListItemIcon>
+                     <DeleteOutlineOutlined />
+                  </ListItemIcon>
+                  <ListItemText>Delete Account</ListItemText>
+               </MenuItem>
+            </MenuList>
+         </Paper>
       </Stack>
    );
 }
