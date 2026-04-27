fbc2663cdf6be19db43d94c221da55e57acc171e
SAOOD <saood-aslam@outlook.com>
2026-04-28 00:01:35 +0100

    readding set states to useDatabase temporarily

--- apps/frontend/src/database/useDatabase.tsx
+++ apps/frontend/src/database/useDatabase.tsx
@@ -12,6 +12,10 @@ type T_Database_Context = {
    morningTasks: T_Task[];
    eveningTasks: T_Task[];
    tags: T_Tag[];
+   setMorningTasks: ReturnType<typeof useLocalStorage<T_Database_Context['morningTasks']>>[1];
+   setEveningTasks: ReturnType<typeof useLocalStorage<T_Database_Context['eveningTasks']>>[1];
+   setTags: ReturnType<typeof useLocalStorage<T_Database_Context['tags']>>[1];
+   setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 const DatabaseContext = createContext<T_Database_Context>({
@@ -19,6 +23,10 @@ const DatabaseContext = createContext<T_Database_Context>({
    morningTasks: [],
    eveningTasks: [],
    tags: [],
+   setMorningTasks: () => {},
+   setEveningTasks: () => {},
+   setTags: () => {},
+   setIsLoading: () => {},
 });
 
 export function DatabaseProvider({ children }: { children: ReactNode }): ReactNode {
@@ -27,7 +35,10 @@ export function DatabaseProvider({ children }: { children: ReactNode }): ReactNo
    const [eveningTasks, setEveningTasks] = useLocalStorage<T_Database_Context['eveningTasks']>('evening-routine-tasks', []);
    const [tags, setTags] = useLocalStorage<T_Database_Context['tags']>('tags', []);
 
-   const value = useMemo(() => ({ isLoading, morningTasks, eveningTasks, tags }), [isLoading, morningTasks, eveningTasks, tags]);
+   const value = useMemo(
+      () => ({ isLoading, morningTasks, eveningTasks, tags, setMorningTasks, setEveningTasks, setTags, setIsLoading }),
+      [isLoading, morningTasks, eveningTasks, tags, setMorningTasks, setEveningTasks, setTags, setIsLoading],
+   );
 
    return <DatabaseContext value={value}>{children}</DatabaseContext>;
 }
