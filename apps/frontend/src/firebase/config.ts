850c9e68537c78573ea2365b968cfcdc7307a986
SAOOD <saood-aslam@outlook.com>
2026-04-28 16:15:58 +0100

    add firestore api

--- apps/frontend/src/firebase/config.ts
+++ apps/frontend/src/firebase/config.ts
@@ -2,6 +2,7 @@
 import { initializeApp, type FirebaseOptions } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
 import { getAuth } from 'firebase/auth';
+import { initializeFirestore, CACHE_SIZE_UNLIMITED, persistentLocalCache, persistentMultipleTabManager } from 'firebase/firestore';
 
 const firebaseConfig: FirebaseOptions = {
    apiKey: 'AIzaSyCw1NGuWWpiZ1J46PFmTmQjqakTshTORlo',
@@ -17,3 +18,7 @@ const firebaseConfig: FirebaseOptions = {
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
+export const db = initializeFirestore(app, {
+   ignoreUndefinedProperties: true,
+   localCache: persistentLocalCache({ cacheSizeBytes: CACHE_SIZE_UNLIMITED, tabManager: persistentMultipleTabManager() }),
+});
