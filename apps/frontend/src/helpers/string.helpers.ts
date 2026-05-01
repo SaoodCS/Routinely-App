8768286ba336e42e2343c5ec8580f6e2bb36c91e
SAOOD <saood-aslam@outlook.com>
2026-05-01 18:39:59 +0100

    stringhelper update

--- apps/frontend/src/helpers/string.helpers.ts
+++ apps/frontend/src/helpers/string.helpers.ts
@@ -17,13 +17,13 @@ const prettierReplace: Record<string, string> = {
 export function formatInputOnSpace(fullInput: string): string {
    // Only format if the last character of the input is a space
    if (!/\s/.test(fullInput.at(-1) ?? '')) return fullInput;
-   // If there are no spaces in updatedInput (i.e. it's just one word), capitalize the first letter and return it:
+   // If there are no spaces in the input (i.e. it's just one word) or if it comes after the end of a sentence (e.g. ". "), capitalize the first letter
    const updatedInput = fullInput.slice(0, -1);
    if (!updatedInput) return fullInput;
-   const hasWhitespace = /\s/.test(updatedInput);
-   if (!hasWhitespace) return `${updatedInput[0].toUpperCase()}${updatedInput.slice(1)} `;
-   // if the last word is a key in the prettierReplace object, return the value of the key:
    const lastWord = updatedInput.trim().split(/\s+/).at(-1) ?? '';
+   const beforeLastWord = updatedInput.slice(0, -lastWord.length);
+   if (!/\s/.test(updatedInput) || /[.!?]\s+$/.test(beforeLastWord)) return `${beforeLastWord}${lastWord[0].toUpperCase()}${lastWord.slice(1)} `;
+   // If the last word is in the prettierReplace object, replace it
    if (prettierReplace[lastWord]) return `${updatedInput.slice(0, -lastWord.length)}${prettierReplace[lastWord]} `;
    return fullInput;
 }
