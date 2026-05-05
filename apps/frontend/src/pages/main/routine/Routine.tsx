566ba56bf63fa97e511cd2a6cfc4bd1f3c79423e
SAOOD <saood-aslam@outlook.com>
2026-05-05 15:44:01 +0100

    update hidden task toggler

--- apps/frontend/src/pages/main/routine/Routine.tsx
+++ apps/frontend/src/pages/main/routine/Routine.tsx
@@ -90,8 +90,13 @@ export default function Routine({ section }: T_RoutineProps): JSX.Element {
          {tags.length > 0 && (
             <AppBar ref={tagHeaderRef} component="div" sx={{ position: 'absolute', height: 'fit-content', border: 'none' }}>
                <Stack spacing={1} direction={'row'} overflow={'auto'} p={1} alignItems={'center'}>
-                  <Chip label={'Toggle All'} onClick={handleToggleAllTags} sx={{ color: 'primary.main' }} variant={'outlined'} />
-                  <Chip label={'Toggle Hidden'} onClick={() => setShowHidden(!showHidden)} sx={{ color: 'primary.main' }} variant={'outlined'} />
+                  <Chip label={'Toggle All Tags'} onClick={handleToggleAllTags} sx={{ color: 'primary.main' }} variant={'outlined'} />
+                  <Chip
+                     label={`${showHidden ? 'Hide' : 'Show'} Hidden Tasks`}
+                     onClick={() => setShowHidden(!showHidden)}
+                     sx={{ color: 'primary.main' }}
+                     variant={'outlined'}
+                  />
                   {tags.map((tag, i) => (
                      <Chip
                         key={tag.id}
